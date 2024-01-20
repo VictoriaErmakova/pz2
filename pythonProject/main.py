c3352176b4b0a748225d989f3b3cb4247a1ffd94
VanoPie <vanop1234@mail.ru>
2024-01-20 11:32:23 +0700

    first commit

---
+++ pythonProject/main.py
@@ -0,0 +1,17 @@
+class SystemBlock:
+    def __init__(self, proccesor, blockpit, matplata):
+        self.proccesor = proccesor
+        self.blockpit = blockpit
+        self.matplata = matplata
+def get_proccesor(self):
+    return self.proccesor
+def set_proccesor(self, proccesor):
+    self.proccesor = proccesor
+def get_blockpit(self):
+    return self.blockpit
+def set_blockpit(self, blockpit):
+    self.blockpit = blockpit
+def get_matplata(self):
+    return self.matplata
+def set_matplata(self, matplata):
+    self.matplata = matplata
